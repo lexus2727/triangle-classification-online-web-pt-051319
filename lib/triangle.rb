6097f8ae10f511ed160d65cd3ac943a15d206f3e
Candice Parker <lexus2424@gmail.com>
2019-06-14 00:19:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,9 @@ class Triangle
   if (@side_1 == @side_2) && (@side_2 == @side_3)
     :equilateral
   
+  elsif
+  (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+  :isosceles
 end
   end
 end
