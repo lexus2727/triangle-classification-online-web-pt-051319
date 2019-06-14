022fdf18cea13111d65375f080f94a257a281c35
Candice Parker <lexus2424@gmail.com>
2019-06-14 00:30:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ class Triangle
     @side_3 = side_3
   end
   
-  def kind
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
   if (@side_1 == @side_2) && (@side_2 == @side_3)
     :equilateral
   
@@ -24,5 +26,8 @@ end
 
 
 class TriangleError < StandardError
+  
+  
+  
 end
 end
